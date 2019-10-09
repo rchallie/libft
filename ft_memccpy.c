3c03688616117d7de795f2c000922370da2487ed
ZeppyYT <56265634+ZeppyYT@users.noreply.github.com>
2019-10-09 19:32:57 +0200

    Add files via upload

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: rchallie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 17:33:26 by rchallie          #+#    #+#             */
-/*   Updated: 2019/10/09 17:52:01 by rchallie         ###   ########.fr       */
+/*   Updated: 2019/10/09 19:32:02 by rchallie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,19 @@
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	unsigned char cuc;
-   	char *dstc;
-	const char *srccc;
+   	unsigned char *dstc;
+	const unsigned char *srccc;
 	size_t i;
 
 	cuc = (unsigned char)c;
-	dstc = (char *)dst;
-	srccc = (const char*)src;
+	dstc = (unsigned char *)dst;
+	srccc = (const unsigned char*)src;
 	i = 0;
 	while (i < n)
 	{
 		dstc[i] = srccc[i];
 		if(srccc[i] == cuc)
-		{
-			dst = dstc;
-			return (dst + i);
-		}
+			return (dst + i + 1);
 		i++;
 	}
 	return (0);
